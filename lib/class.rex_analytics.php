7487f8e25fb0b7bd6e7140149e3cbe37e958633f
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2016-03-04 13:52:37 +0100

    Fixes #4

--- lib/class.rex_analytics.php
+++ lib/class.rex_analytics.php
@@ -5,23 +5,25 @@
 		}
 		
 		public static function output($tags = true) {
-			$code = '';
-			if ($tags) {
-				$code .= "<script>".PHP_EOL;
+			if (!rex_backend_login::hasSession()) {
+				$code = '';
+				if ($tags) {
+					$code .= "<script>".PHP_EOL;
+				}
+				$code .= "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){".PHP_EOL;
+				$code .= "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),".PHP_EOL;
+				$code .= " m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)".PHP_EOL;
+				$code .= "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');".PHP_EOL;
+				$code .= "".PHP_EOL;
+				$code .= "ga('create', '".self::getCode()."', 'auto');".PHP_EOL;
+				$code .= "ga('send', 'pageview');".PHP_EOL;
+				
+				if ($tags) {
+					$code .= "</script>".PHP_EOL;
+				}
+				
+				return $code;
 			}
-			$code .= "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){".PHP_EOL;
-			$code .= "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),".PHP_EOL;
-			$code .= " m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)".PHP_EOL;
-			$code .= "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');".PHP_EOL;
-			$code .= "".PHP_EOL;
-			$code .= "ga('create', '".self::getCode()."', 'auto');".PHP_EOL;
-			$code .= "ga('send', 'pageview');".PHP_EOL;
-			
-			if ($tags) {
-				$code .= "</script>".PHP_EOL;
-			}
-			
-			return $code;
 		}
 	}
 ?>
