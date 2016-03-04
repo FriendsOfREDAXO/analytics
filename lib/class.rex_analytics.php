30c1491894532b56b547fab222e3322a9142d1c2
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2016-03-04 14:37:04 +0100

    Fixes #1

--- lib/class.rex_analytics.php
+++ lib/class.rex_analytics.php
@@ -10,13 +10,10 @@
 				if ($tags) {
 					$code .= "<script>".PHP_EOL;
 				}
-				$code .= "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){".PHP_EOL;
-				$code .= "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),".PHP_EOL;
-				$code .= " m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)".PHP_EOL;
-				$code .= "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');".PHP_EOL;
-				$code .= "".PHP_EOL;
-				$code .= "ga('create', '".self::getCode()."', 'auto');".PHP_EOL;
-				$code .= "ga('send', 'pageview');".PHP_EOL;
+				
+				$fragment = new rex_fragment();
+				$fragment->setVar('code', self::getCode(), false);
+				$code .= $fragment->parse('rex_analytics/embedcode.php');
 				
 				if ($tags) {
 					$code .= "</script>".PHP_EOL;
