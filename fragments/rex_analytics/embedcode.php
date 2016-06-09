b283d175aadc51037731d4062917a83ebf1d310f
Thomas Kaegi <tk@meeloo.ch>
2016-06-09 15:32:10 +0200

    OptOutCookie - Fehlerbehebung

--- fragments/rex_analytics/embedcode.php
+++ fragments/rex_analytics/embedcode.php
@@ -4,6 +4,9 @@
 		var gaProperty = '<?=$this->code;?>';
 		var disableStr = 'ga-disable-' + gaProperty;
 		if (document.cookie.indexOf(disableStr + '=true') > -1) {
+			window[disableStr] = true;
+		}
+		function gaOptout() {
 			document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
 			window[disableStr] = true;
 		}
